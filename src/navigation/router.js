29f56aaf38661ca7899a97d27690998d3c0186ba
Enfec Solutions <banidazin33@gmail.com>
2022-06-25 15:30:54 +0530

    add stack navigator

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {BuildScreen} from '../screens';
+
+const Stack = createNativeStackNavigator();
 
 export const Router = () => {
   return (
-    <View>
-      <Text>Router</Text>
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Build" component={BuildScreen} />
+    </Stack.Navigator>
   );
 };
